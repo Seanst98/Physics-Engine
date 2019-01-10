b365e56673335d3bef6fbf8dd10cf07e078920be
Sean Stirling <sean.a.stirling@gmail.com>
2019-01-10 22:04:52 +0000

    NSquared header and cpp files created

---
+++ NSquared.cpp
@@ -0,0 +1,27 @@
+#include <list>
+#include "Object.h"
+#include "NSquared.h"
+
+const std::list<std::pair<Object*, Object*>> &NSquared::ComputePairs()
+{
+	m_pairs.clear();
+
+	//outer loop
+	auto end = m_aabbs.end();
+	for (auto i = m_aabbs.begin(); i != end; ++i)
+	{
+
+		//inner loop
+		auto jStart = i;
+		for (auto j = ++jStart; j != end; ++j)
+		{
+			AABB = *aabbA = *i;
+			AABB = *aabbB = *j;
+			Object *objectA = aabbA->Object();
+			Object *objectB = aabbB->Object();
+
+			//skip same body collision
+			if ()
+		}
+	}
+}
---
+++ NSquared.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "BroadPhase.h"
+
+class NSquared : public BroadPhase
+{
+public:
+
+	virtual void Add(AABB *aabb)
+	{
+		m_aabbs.push_back(aabb);
+	}
+
+	virtual void Update()
+	{
+		//do nothing
+	}
+
+	virtual std::list<std::pair<Object*, Object*>> &ComputePairs();
+	virtual Object *Pick(const sf::Vector2f &point) const;
+	virtual void Query(const AABB &aabb, std::vector<Object*> &out) const;
+	virtual RayCastResult RayCast(const Ray2 &ray) const;
+
+private:
+
+	std::list<AABB*> m_aabbs;
+	std::list<std::pair<Object*, Object*>> m_pairs;
+
+
+};
