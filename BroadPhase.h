e5113c5f3fa3460abdcd1db8988d97e3af48f5c9
Sean Stirling <sean.a.stirling@gmail.com>
2019-01-11 20:16:02 +0000

    Created the BroadPase class

---
+++ BroadPhase.h
@@ -0,0 +1,43 @@
+#pragma once
+#include "AABB.h"
+
+class BroadPhase
+{
+public:
+
+	//adds a new AABB to the broadphase
+	virtual void Add(AABB *aabb) = 0;
+
+	//updates broadphase to react to changes to AABB
+	virtual void Update() = 0;
+
+	//returns a list of possibly colliding objects
+	virtual const std::list<std::pair<Object*, Object*>> &ComputePairs() = 0;
+
+	//returns an object that collides with a point
+	//returns null if no object exists
+	virtual Object *Pick(const sf::Vector2f &point) const = 0;
+
+	//returns a list of objects whos AABBs collide with a query AABB
+	virtual void Query(const AABB &aabb, std::list<Object*> &output) const = 0;
+
+	//result contains the first object the ray hits
+	//result contains null if no object is hit
+	virtual RayCastResult RayCast(const Ray2 &ray) const = 0;
+
+};
+
+struct Ray2
+{
+	sf::Vector2f pos;
+	sf::Vector2f dir;
+
+};
+
+struct RayCastResult
+{
+	bool hit;
+	Object *object;
+	sf::Vector2f position;
+	sf::Vector2f normal;
+};
--- NSquared.cpp
+++ NSquared.cpp
@@ -1,6 +1,7 @@
 #include <list>
 #include "Object.h"
 #include "NSquared.h"
+#include "AABB.h"
 
 const std::list<std::pair<Object*, Object*>> &NSquared::ComputePairs()
 {
@@ -15,8 +16,8 @@ const std::list<std::pair<Object*, Object*>> &NSquared::ComputePairs()
 		auto jStart = i;
 		for (auto j = ++jStart; j != end; ++j)
 		{
-			AABB = *aabbA = *i;
-			AABB = *aabbB = *j;
+			AABB *aabbA = *i;
+			AABB *aabbB = *j;
 			Object *objectA = aabbA->Object();
 			Object *objectB = aabbB->Object();
 
