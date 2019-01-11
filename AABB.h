bd1ecd56740868499a8bea2723a2f575893cd543
Sean Stirling <sean.a.stirling@gmail.com>
2019-01-11 15:32:19 +0000

    Added the AABB class

---
+++ AABB.h
@@ -0,0 +1,78 @@
+#pragma once
+#include "stdafx.h"
+
+class AABB
+{
+public:
+
+	float minX;
+	float minY;
+	float maxX;
+	float maxY;
+	float surfaceArea;
+
+	AABB() {
+		minX = 0;
+		minY = 0;
+		maxX = 0;
+		maxY = 0;
+		surfaceArea = 0;
+	}
+
+	AABB(float minx, float miny, float maxx, float maxy) {
+		minX = minx;
+		minY = miny;
+		maxX = maxx;
+		maxY = maxy;
+
+		surfaceArea = CalculateSurfaceArea();
+	}
+
+	AABB(float minx, float miny, float maxx, float maxy, float srfcarea) {
+		minX = minx;
+		minY = miny;
+		maxX = maxx;
+		maxY = maxy;
+
+		surfaceArea = srfcarea;
+	}
+
+	bool overlaps(const AABB& other) const
+	{
+		return maxX > other.maxX &&
+			minX < other.maxX &&
+			maxY > other.minY &&
+			minY < other.maxY;
+	}
+
+	bool containsAABB(const AABB& other) const
+	{
+		return maxX >= other.maxX &&
+			minX <= other.maxX &&
+			maxY >= other.minY &&
+			minY <= other.maxY;
+	}
+
+	AABB merge(const AABB& other) const
+	{
+		return AABB(
+			std::min(minX, other.minX), std::min(minY, other.minY),
+			std::max(maxX, other.maxX), std::max(maxY, other.maxY)
+		);
+	}
+
+	AABB intersection(const AABB& other) const
+	{
+		return AABB(
+			std::max(minX, other.minX), std::max(minY, other.minY),
+			std::min(maxX, other.maxX), std::min(maxY, other.maxY)
+		);
+	}
+
+	float CalculateSurfaceArea() {
+		return (getWidth()*getHeight());
+	}
+
+	float getWidth() const { return maxX - minX; }
+	float getHeight() const { return maxY - minY; }
+};
